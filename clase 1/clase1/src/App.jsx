ad64702e0f8f6ef0dfeef833d0a10e60b5ebda2a
SantiagoRibot96 <85650377+santiagoribot@users.noreply.github.com>
2023-09-23 15:48:36 -0300

    primera clase

--- clase 1/clase1/src/App.jsx
+++ clase 1/clase1/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ProductCard from './components/ProductCard/ProductCard'
 
 function App() {
 
 
   return (
     <>
-      <h1>Hola mundo!</h1>
+      <ProductCard/>
     </>
   )
 }
---
+++ clase 1/clase1/src/components/ProductInfo/ProductInfo.jsx
@@ -0,0 +1,13 @@
+import './ProductInfo.css'
+
+const ProductInfo = () => {
+  return (
+    <div className='product-info'>
+        <h1>Producto</h1>
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione quos aut natus illum officiis expedita necessitatibus accusamus praesentium provident, non sed velit laudantium veniam mollitia ipsam nulla, dolorem nobis possimus minima eveniet alias. Veniam minus quasi explicabo iste aperiam temporibus error numquam dolorem. Commodi et doloremque quia quas inventore, amet, possimus qui libero expedita quaerat ipsum eos deleniti aliquam voluptatem, accusamus sunt sapiente? Explicabo reiciendis temporibus quas sit, distinctio numquam sequi repellendus recusandae cumque sint labore, voluptatem accusantium nesciunt, aut dolore consequuntur dicta magnam maxime voluptatibus animi. Iure dolorum nostrum et accusantium cum ea quisquam, itaque in repellendus tempore dolor.</h2>
+        <p>$100</p>
+    </div>
+  )
+}
+
+export default ProductInfo
