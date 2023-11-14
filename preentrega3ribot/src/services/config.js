656e2cb1fb446c58427f4f6a3ff5c7b136714643
SantiagoRibot96 <85650377+santiagoribot@users.noreply.github.com>
2023-11-14 19:41:51 -0300

    }entrega final

--- preentrega3ribot/src/services/config.js
+++ preentrega3ribot/src/services/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAte66wr2gkug1kPYQJWoi7M6FFlI8_UNo",
-  authDomain: "todoparrillas-be4ea.firebaseapp.com",
-  projectId: "todoparrillas-be4ea",
-  storageBucket: "todoparrillas-be4ea.appspot.com",
-  messagingSenderId: "73368551427",
-  appId: "1:73368551427:web:887bd4e89df83c1f590e36"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
